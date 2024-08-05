e47568bd762024deade802aaa2f1faaba01f9009
leechulwon <asfree@naver.com>
2024-08-05 09:13:56 +0900

    메인수정

--- SampleWeb/src/main/java/com/controller/SecondController.java
+++ SampleWeb/src/main/java/com/controller/SecondController.java
@@ -27,6 +27,7 @@ public class SecondController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("second Controller");
+		System.out.println("second Controller main");
 	}
 
 	/**
